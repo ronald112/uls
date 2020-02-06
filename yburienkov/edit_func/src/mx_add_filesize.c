2c1d01bffa9b8c3512c41ac19a80b4f8d098182b
ronald <ronald@ronalds-Mac.local>
2020-02-06 15:50:25 +0200

    yburienkov //fix time output\\

--- yburienkov/edit_func/src/mx_add_filesize.c
+++ yburienkov/edit_func/src/mx_add_filesize.c
@@ -1,9 +1,11 @@
 #include "uls.h"
 
 void mx_add_filesize(off_t size, t_catalog *cat, char **result) {
-    char *temp = mx_itoa(size);
+    char *temp = mx_ltoa(size);
     long long cnt = 0;
 
+    if (size == 0)
+        cnt = 1;
     *result = mx_addstr(*result, "  ");
     for (; size != 0; size /= 10, cnt++);
     cnt = cat->max_size_ofdir - cnt;

8c430e9a24f6fff880440d8d5935923dd5de9f34
ronald112 <ronald112@.github.com>
2020-02-09 13:59:31 +0200

    yburienkov //fix libmx auditor\\

--- yburienkov/edit_func/libmx/inc/libmx.h
+++ yburienkov/edit_func/libmx/inc/libmx.h
@@ -98,7 +98,7 @@ char *mx_replace_substr(const char *str, const char *sub, const char *replace);
 void *mx_memset(void *b, int c, size_t len);
 void *mx_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void *mx_memccpy(void *restrict dst, const void *restrict src,
-                 int c, size_t n);
+                int c, size_t n);
 int mx_memcmp(const void *s1, const void *s2, size_t n);
 void *mx_memchr(const void *s, int c, size_t n);
 void *mx_memrchr(const void *s, int c, size_t n);

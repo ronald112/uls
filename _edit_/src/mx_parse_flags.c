e217a35ac0687b8e14798c2687259974ed676a98
ronald1121 <ronald11211@gmail.com>
2020-03-21 03:42:37 -0700

    yb //add usage, fix parser\\

--- _edit_/src/mx_parse_flags.c
+++ _edit_/src/mx_parse_flags.c
@@ -1,43 +1,65 @@
 #include "uls.h"
 
-static void chk_l1Cf_flags(char c, t_main *info) {
-    if (c == 'l') {
+static void print_usage(bool chk_flag, char illegel_opt) {
+    if (chk_flag == 0) {
+        mx_printerr("uls: illegal option -- ");
+        mx_printerr(&illegel_opt);
+        mx_printerr("\nusage: uls [-@ACRafhl1] [file ...]\n");
+        exit(1);
+    }
+}
+
+static bool chk_l1C_flags(char c, t_main *info) {
+    bool chk_flag = 0;
+
+    if ((c == 'l') && (chk_flag = 1)) {
         info->flag.is_l = true;
         info->flag.is_1 = false;
         info->flag.is_C = false;
     }
-    else if (c == '1') {
+    else if ((c == '1') && (chk_flag = 1)) {
         info->flag.is_l = false;
         info->flag.is_1 = true;
         info->flag.is_C = false;
     }
-    else if (c == 'C') {
+    else if ((c == 'C') && (chk_flag = 1)) {
         info->flag.is_l = false;
         info->flag.is_1 = false;
         info->flag.is_C = true;
     }
-    else if (c == 'f') {
+    return chk_flag;
+}
+
+static bool chk_ARaf_flag(char c, t_main *info, bool chk_l1C) {
+    bool chk_flag = chk_l1C;
+
+    if ((c == 'a') && (chk_flag = 1)) {
+        info->flag.is_a = true;
+        info->flag.is_A = false;
+    }
+    else if ((c == 'A') && (chk_flag = 1))
+        info->flag.is_A = true;
+    if ((c == 'R') && (chk_flag = 1))
+        info->flag.is_R = true;
+    if ((c == 'f') && (chk_flag = 1)) {
         info->flag.is_f = true;
         info->flag.is_a = true;
         info->flag.is_C = false;
     }
+    return chk_flag;
 }
 
+ // n, B, v
 static void chk_line_for_flags(char *argv, t_main *info) {
-    for (int i = 0; argv[i] != '\0'; ++i) {
-        chk_l1Cf_flags(argv[i], info);
-        if (argv[i] == 'a') {
-            info->flag.is_a = true;
-            info->flag.is_A = false;
-        }
-        else if (argv[i] == 'A')
-            info->flag.is_A = true;
-        if (argv[i] == 'R')
-            info->flag.is_R = true;
-        if (argv[i] == 'h')
+    for (int i = 1; argv[i] != '\0'; ++i) {
+        bool chk_flag = chk_l1C_flags(argv[i], info);
+
+        chk_flag = chk_ARaf_flag(argv[i], info, chk_flag);
+        if ((info->flag.is_l == true) && (argv[i] == 'h') && (chk_flag = 1))
             info->flag.is_h = true;
-        if (argv[i] == '@')
+        if ((info->flag.is_l == true) && (argv[i] == '@') && (chk_flag = 1))
             info->flag.is_dog = true;
+        print_usage(chk_flag, argv[i]);
     }
 }
 
